cb561a4256d6d75a063da932739f38702bafd997
kosohmar <kosohmar@fel.cvut.cz>
2023-12-11 18:09:20 +0100

    Change plotting function

--- src/utils.jl
+++ src/utils.jl
@@ -1,26 +1,33 @@
 using Plots
 
-function plot_trajectory2D(min_max, trajectory, min_bound, max_bound)
+function plot_trajectory2D(min_max, trajectory, fst_bounds, snd_bounds)
     x1_coords = [pt[1] for pt in trajectory]
     x2_coords = [pt[2] for pt in trajectory]
+    fst = abs(fst_bounds[1]-fst_bounds[2])/20
+    snd = abs(snd_bounds[1]-snd_bounds[2])/20
+    x1_min_bound = fst_bounds[1] - fst
+    x1_max_bound = fst_bounds[2] + fst
+    x2_min_bound = snd_bounds[1] - snd
+    x2_max_bound = snd_bounds[2] + snd
     scatter(x1_coords, x2_coords; color=:blue, legend = false, markersize = 1,
-        markershape=:auto, xlims = [min_bound-0.1, max_bound+0.1], ylims = [min_bound-0.1, max_bound+0.1])
+        markershape=:auto, xlims = [x1_min_bound, x1_max_bound], ylims = [x2_min_bound, x2_max_bound])
     scatter!([min_max[1]], [min_max[2]], markershape=:star, markersize=10, color=:blue)
     title!("Trajectory")
 end
 
-function plot_contour2D(min_max, f, min_bound, max_bound)
+function plot_contour2D(min_max, f, fst_bounds, snd_bounds)
     f_plot(x,y) = f([x,y])
-    x1 = range(min_bound, max_bound, length=300)
-    x2 = range(min_bound, max_bound, length=300)
+    x1 = range(fst_bounds[1], fst_bounds[2], length=300)
+    x2 = range(snd_bounds[1], snd_bounds[2], length=300)
     contourf(x1, x2, f_plot, levels=50)
     scatter!([min_max[1]], [min_max[2]], markershape=:star, markersize=10, legend=false, color=:blue)
     title!("Contour plot of the function")
 end
 
-function plot_surface(min_max, f, min_bound, max_bound)
+function plot_surface(min_max, f, fst_bounds, snd_bounds)
     f_plot(x,y) = f([x,y])
-    xs = ys = range(min_bound, max_bound, length=100)
+    xs = range(fst_bounds[1], fst_bounds[2], length=100)
+    ys = range(snd_bounds[1], snd_bounds[2], length=100)
     zs = [f_plot(x,y) for y in ys, x in xs]
     surface(xs, ys, zs, legend=false)
     scatter!([min_max[1]], [min_max[2]], [f(min_max)], markershape=:star, markersize=10, legend=false, color=:blue)
